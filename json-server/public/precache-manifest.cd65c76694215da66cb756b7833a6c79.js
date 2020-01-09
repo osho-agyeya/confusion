544c5f4f7a66fe0faeffb9165368c7b0ccea3e96
Osho Agyeya <oshoagyeya123@gmail.com>
2020-01-09 05:35:48 +0530

    json server added

---
+++ json-server/public/precache-manifest.cd65c76694215da66cb756b7833a6c79.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea758869e63cf66e7ef12eb4ac0ad5dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383cbc4aa7287ec3e055",
+    "url": "/static/css/2.84c661cf.chunk.css"
+  },
+  {
+    "revision": "9a8cb6ed3557f9a21b12",
+    "url": "/static/css/main.34c567d8.chunk.css"
+  },
+  {
+    "revision": "383cbc4aa7287ec3e055",
+    "url": "/static/js/2.0b6265c9.chunk.js"
+  },
+  {
+    "revision": "9a8cb6ed3557f9a21b12",
+    "url": "/static/js/main.4753ed7d.chunk.js"
+  },
+  {
+    "revision": "c5a0eb96521c386a5103",
+    "url": "/static/js/runtime-main.6a3a5a48.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
